c64dba6339747287b8b5538645fb54ec4dd93e93
ragnar-vallhala <ashutoshvishwakarma208@outlook.com>
2025-11-17 15:37:14 +0530

    Added static page

--- backend/urls.py
+++ backend/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/auth/login/', TokenObtainPairView.as_view()),
     path('api/auth/refresh/', TokenRefreshView.as_view()),
+    path("", TemplateView.as_view(template_name="site/index.html")),
+
 ]
